c9c7d30facd9eee4227d349e7d7eba5991c91d65
sgrg007 <sgrg007@gmail.com>
2022-03-20 10:07:43 -0500

    initial commit

---
+++ src/main/java/com/gurung/awskinesisconsumer/AwsKinesisConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.gurung.awskinesisconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class AwsKinesisConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsKinesisConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gurung/awskinesisconsumer/AwsKinesisConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurung.awskinesisconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsKinesisConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
