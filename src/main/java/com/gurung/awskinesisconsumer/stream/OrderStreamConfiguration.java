767ab2909fcb3bea1dcf954c9e54a1f2a93f1f8f
sgrg007 <sgrg007@gmail.com>
2022-03-21 21:58:36 -0500

    added logs

--- src/main/java/com/gurung/awskinesisconsumer/stream/OrderStreamConfiguration.java
+++ src/main/java/com/gurung/awskinesisconsumer/stream/OrderStreamConfiguration.java
@@ -1,6 +1,7 @@
 package com.gurung.awskinesisconsumer.stream;
 
 import com.gurung.awskinesisconsumer.entity.Event;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +13,13 @@ import java.util.function.Consumer;
  * @since 3/21/2022
  */
 @Component
+@Slf4j
 public class OrderStreamConfiguration {
     @Bean
     public Consumer<Event> processOrder(){
         return event -> {
-            System.out.println("Order from the kinesis stream:");
-            System.out.println(event.getOrder().toString());
+            log.info("Order from the kinesis stream:");
+            log.info(event.getOrder().toString());
         };
     }
 }
